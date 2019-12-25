07f49ffb3774c47f5742a08cff8284be45764068
Marcin Kubiak <hugo26@o2.pl>
2019-12-25 12:51:35 +0100

    Refactor List component into functional

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,41 +7,37 @@ import Creator from '../Creator/Creator.js';
 import { settings } from '../../data/dataStore';
 import styles from './List.scss';
 
-class List extends React.Component {
-  static propTypes = {
-    title: PropTypes.node.isRequired,
-    imgSrc: PropTypes.string.isRequired,
-    description: PropTypes.node,
-    columns: PropTypes.array,
-    listIndex: PropTypes.string,
-    image: PropTypes.string,
-    addColumn: PropTypes.func,
-  };
+const List = ({ title, image, description, columns, addColumn }) => {
+  return (
+    <section className="styles.component">
+      <Hero titleText={title} image={image} />
+      <div className={styles.description}>{ReactHtmlParser(description)}</div>
+      <div className={styles.columns}>
+        {columns.map(columnData => (
+          <Column key={columnData.id} {...columnData} />
+        ))}
+      </div>
+      <div className={styles.creator}>
+        <Creator text={settings.columnCreatorText} action={addColumn} />
+      </div>
+    </section>
+  );
+};
 
-  static defaultProps = {
-    description: settings.defaultListDescription,
-    imgSrc:
-      'https://images.pexels.com/photos/3345270/pexels-photo-3345270.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-  };
+List.propTypes = {
+  title: PropTypes.node.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  description: PropTypes.node,
+  columns: PropTypes.array,
+  listIndex: PropTypes.string,
+  image: PropTypes.string,
+  addColumn: PropTypes.func,
+};
 
-  render() {
-    const { title, image, description, columns, addColumn } = this.props;
-
-    return (
-      <section className="styles.component">
-        <Hero titleText={title} image={image} />
-        <div className={styles.description}>{ReactHtmlParser(description)}</div>
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn} />
-        </div>
-      </section>
-    );
-  }
-}
+List.defaultProps = {
+  description: settings.defaultListDescription,
+  imgSrc:
+    'https://images.pexels.com/photos/3345270/pexels-photo-3345270.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+};
 
 export default List;
